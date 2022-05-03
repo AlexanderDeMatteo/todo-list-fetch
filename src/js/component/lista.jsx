51b320e9b84a55434a99ebe52a891f242180d2b4
AlexanderDeMatteo <aldml0201@gmail.com>
2022-05-03 00:58:41 +0000

    ajuste

--- src/js/component/lista.jsx
+++ src/js/component/lista.jsx
@@ -4,15 +4,16 @@ export const Notes = () => {
 	const [addTodolist, setAddTodolist] = useState("");
 	const [todoList, setTodolist] = useState([]);
 	const [tareasPendientes, settareasPendientes] = useState("");
+
 	const handleList = (e) => {
 		if (e.key === "Enter" && addTodolist != "") {
-			setTodolist([...todoList, addTodolist]);
-			setAddTodolist("");
 			if (tareasPendientes == "") {
 				settareasPendientes(1);
 			} else {
 				settareasPendientes(tareasPendientes + 1);
 			}
+			setTodolist([...todoList, addTodolist]);
+			setAddTodolist("");
 		}
 	};
 
@@ -23,6 +24,34 @@ export const Notes = () => {
 		if (tareasPendientes - 1 == 0) {
 			settareasPendientes("");
 		}
+	};
+
+	const deleteAll = (position) => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr", {
+			method: "DELETE",
+			body: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				if (resp.status == 404) {
+					setTodolist([]);
+				}
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//here is were your code should start after the fetch finishes
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch((error) => {
+				//error handling
+				console.log("error");
+				console.log(error);
+			});
 	};
 
 	return (
@@ -41,7 +70,7 @@ export const Notes = () => {
 					{todoList.map((todolist, index) => {
 						return (
 							<div className="Task" key={index}>
-								<span>{todolist}</span>
+								<span>{todolist.label}</span>
 								<button
 									type="button"
 									className="btn-close"
@@ -58,6 +87,12 @@ export const Notes = () => {
 					<>{tareasPendientes != "" ? "tareas por realizar" : ""}</>
 					{tareasPendientes}
 				</div>
+				<button
+					onClick={() => {
+						deleteAll();
+					}}>
+					borrador
+				</button>
 			</div>
 		</div>
 	);
